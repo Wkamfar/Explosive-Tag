ab534454018529869c9c033138f4c01cd8f7951d
Wkamfar <wkamfar@gmail.com>
2021-08-08 11:37:02 -0400

    We made the game tag

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -7,16 +7,33 @@ public class CharacterController : MonoBehaviour
     public GameObject playerModel;
     public float speed;
     public PhotonView view;
+    private bool isTagged;
+    private float tagTimer;
+    public Color taggedColor;
+    public Color nonTaggedColor;
     // Start is called before the first frame update
     void Start()
     {
         view = GetComponent<PhotonView>();
+        Debug.Log("CharacterController.Start" + view.ViewID);
+        if (view.ViewID == 1001)
+        {
+            isTagged = true;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         CheckInput();
+        if (isTagged == true)
+        {
+            this.gameObject.GetComponent<Renderer>().material.color = taggedColor;
+        }
+        else if (isTagged == false)
+        {
+            this.gameObject.GetComponent<Renderer>().material.color = nonTaggedColor;
+        }
     }
     void CheckInput()
     {
@@ -32,6 +49,20 @@ public class CharacterController : MonoBehaviour
             if (isBack) playerModel.GetComponent<Rigidbody>().AddForce(this.transform.forward * -speed * 1000 * Time.deltaTime);
         }
         
+    }
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Player") && isTagged && col.gameObject.GetComponent<CharacterController>().tagTimer <= 0)
+        {
+            col.gameObject.GetComponent<CharacterController>().isTagged = true;
+            isTagged = false;
+            tagTimer = 3;
+            Invoke("resetTagTimer", tagTimer);
+        }
+    }
+    private void resetTagTimer()
+    {
+        tagTimer = 0;
     }
 }
 
